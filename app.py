a585b6ea30acff4346d54b33f408817f4dd53ff3
Amirsfaxi <130224948+Amirsfaxi@users.noreply.github.com>
2025-05-10 17:29:34 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+# save as app.py
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from torchvision import models, transforms
+import torch
+import torch.nn as nn
+from PIL import Image
+import io
+
+app = FastAPI()
+
+# Class labels
+class_names = ['Flea_Allergy', 'Health', 'Ringworm', 'Scabies']
+
+# Load model
+model = models.resnet50(weights=None)
+model.fc = nn.Linear(model.fc.in_features, len(class_names))
+model.load_state_dict(torch.load("C:/Users/medam/Downloads/best_model.pth", map_location="cpu"))
+model.eval()
+
+# Image transform
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = Image.open(io.BytesIO(await file.read())).convert("RGB")
+    img_tensor = transform(image).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(img_tensor)
+        _, pred = torch.max(output, 1)
+
+    result = class_names[pred.item()]
+    return JSONResponse({"prediction": result})
